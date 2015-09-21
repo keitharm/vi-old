b94f915b02676ff21b41205def6539f75381b3d5
Garrett Maring <garrett.maring@gmail.com>
2015-09-21 15:42:03 -0700

    Multiple arguments from string inputs mapped to function args

--- viapi/test/apitest.js
+++ viapi/test/apitest.js
@@ -55,41 +55,38 @@ describe('Helper Functions', function(){
   describe(' - attemptCommand - ', function(){
     var commands = {
       'hi': function(err) {
-        //Here!
+        return 'hi';
+      },
+      'hello $1': function(err, arg){
+        return 'hello ' + arg;
+      },
+      'how is the $1 in $2': function(err, thing, place){
+        return "The " + thing + " in " + place + " is great!";
       }
     };
-    
     it('should return false if command not found', function(){
-
       return expect(Vi.helpers.attemptCommand('bye', commands)).to.equal(false);
     });
-    it('should invoke the command if found', function(){
-
+    it('should return value and invoke function if the command is found', function(){
+      return expect(Vi.helpers.attemptCommand('hi', commands)).to.equal('hi');
+    });
+    it('should return correct value with argument', function(){
+      return expect(Vi.helpers.attemptCommand('hello john', commands)).to.equal('hello john');
+    });
+    it('should return correct value with arguments', function(){
+      return expect(Vi.helpers.attemptCommand('how is the weather in san francisco', commands)).to.equal('The weather in san francisco is great!');
     });
   });
 
-  describe(' - parseCommand - ', function(){
-
-  });
-
-  describe(' - parseCommands - ', function(){
-
-  });
-
-  describe(' - callCommandWithText - ', function(){
-
-  });
-});
-
-describe('Build', function(){
-  xit('should have a buildApplication function', function(){
-    var myApp = new Vi.App({
-      title: 'my app',
-      commands: {}
+  describe(' - inputToArgumentsArray - ', function(){
+    var input = 'hey there john how are you doing in sf';
+    var command = 'hey there $1 how are you doing in $2';
+    it('should get an array of the final arguments', function(){
+      var args = Vi.helpers.inputToArgumentsArray(command, input);
+      expect(args).to.be.a('array');
+      expect(args[0]).to.equal('john');
+      expect(args[1]).to.equal('sf');
+      return;
     });
-    return expect(myApp.buildApp).to.be.a('function');
-  });
-  xit('should have same number of commands', function(){
-    // return expect(Vi.Build.commands).to.be.same.length.as(Vi.App.commands);
   });
 });
