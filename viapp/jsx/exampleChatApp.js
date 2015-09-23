82aff19fc7a6759d18e90fc9946ca23fc84b7941
Garrett Maring <garrett.maring@gmail.com>
2015-09-22 18:18:58 -0700

    Docs updated

--- viapp/jsx/exampleChatApp.js
+++ viapp/jsx/exampleChatApp.js
@@ -12,14 +12,13 @@ var chat = new Vi.App({
     /* No arguments */
     "hi": function(err, voice){
       console.log('hi command triggered!');
+      voice('hi');
     },
-
     /* Single trailing argument */
     "hello $1": function(err, voice, name){
       console.log('hello ' + name);
       voice("hello " + name);
     },
-
     /* Multiple argument with break word declared */
     "say $1 to $2": function(err, voice, messsage, user){
       console.log('say ' + message + " to " + user);
