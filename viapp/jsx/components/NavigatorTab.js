e8a8abf09566e57136fa525f486a3078c2cca94e
Eugene Lee <colemakdvorak@gmail.com>
2015-10-15 10:22:08 -0700

    (etc) Style/indentation cleanup

--- viapp/jsx/components/NavigatorTab.js
+++ viapp/jsx/components/NavigatorTab.js
@@ -1,72 +1,73 @@
 var React = require('react-native'),
-	Home = require('./Home.js'),
-	Search = require('./Search.js'),
-    MyApps = require('./MyApps.js');
+  Home = require('./Home.js'),
+  Search = require('./Search.js'),
+  MyApps = require('./MyApps.js'),
+  Settings = require('./Settings.js');
 
 var {
-	TabBarIOS
+  TabBarIOS
 } = React;
 
 var TabBarItemIOS = TabBarIOS.Item
 
 var NavigatorTab = React.createClass({
-	componentWillMount: function(){
-  	},
-  	componentWillUnmount: function(){
-  	},
-  	componentDidMount: function(){
-  	},
-  	getInitialState: function(){
-    	return {
-    		selectedTab: 'home'
-    	};
-  	},
-	render: function(){
-		return (
-			<TabBarIOS selectedTab = {this.state.selectedTab}>
-				<TabBarItemIOS
-					selected = {this.state.selectedTab === 'home'}
-					systemIcon = 'featured'
-					onPress = {() => {
-						this.setState({
-							selectedTab: 'home'
-						});
-					}}>
-				<Home />
-				</TabBarItemIOS>
-				<TabBarItemIOS
-					selected = {this.state.selectedTab === 'myapps'}
-					systemIcon = 'bookmarks'
-					onPress = {() => {
-						this.setState({
-							selectedTab: 'myapps'
-						});
-					}}>
-				<MyApps />
-				</TabBarItemIOS>
-		        <TabBarItemIOS
-		         selected = {this.state.selectedTab === 'search'}
-		         systemIcon = 'search'
-		         onPress = {() => {
-		           this.setState({
-		             selectedTab: 'search'
-		           });
-		         }}>
-		         <Search />
-		        </TabBarItemIOS>
+  componentWillMount: function(){
+  },
+  componentWillUnmount: function(){
+  },
+  componentDidMount: function(){
+  },
+  getInitialState: function(){
+    return {
+  		selectedTab: 'home'
+  	};
+  },
+  render: function(){
+    return (
+      <TabBarIOS selectedTab = {this.state.selectedTab}>
+        <TabBarItemIOS
+          selected = {this.state.selectedTab === 'home'}
+          systemIcon = 'featured'
+          onPress = {() => {
+            this.setState({
+              selectedTab: 'home'
+            });
+        }}>
+          <Home />
+        </TabBarItemIOS>
+        <TabBarItemIOS
+          selected = {this.state.selectedTab === 'myapps'}
+          systemIcon = 'bookmarks'
+          onPress = {() => {
+            this.setState({
+              selectedTab: 'myapps'
+            });
+          }}>
+          <MyApps />
+        </TabBarItemIOS>
+        <TabBarItemIOS
+          selected = {this.state.selectedTab === 'search'}
+          systemIcon = 'search'
+          onPress = {() => {
+            this.setState({
+              selectedTab: 'search'
+            });
+        }}>
+          <Search />
+        </TabBarItemIOS>
+        <TabBarItemIOS
+          selected = {this.state.selectedTab === 'settings'}
+          systemIcon = 'more'
+          onPress = {() => {
+            this.setState({
+              selectedTab: 'settings'
+            });
+          }}>
+          <Settings />
+		    </TabBarItemIOS>
 			</TabBarIOS>
 		);
 	}
 });
 
 module.exports = NavigatorTab;
-
-// // <TabBarItemIOS
-// // 	selected = {this.state.selectedTab === 'settings'}
-// // 	systemIcon = 'more'
-// // 	onPress = {() => {
-// // 		this.setState({
-// // 			selectedTab: 'settings'
-// // 		});
-// // 	}}>
-// // </TabBarItemIOS>
--- viapp/jsx/components/Settings.js
+++ viapp/jsx/components/Settings.js
@@ -1,49 +1,51 @@
 var React = require('react-native')
 
 var {
-	View,
-	SwitchIOS,
-	Text
+  View,
+  SwitchIOS,
+  Text
 } = React;
 
 var Settings = React.createClass({
-	componentWillMount: function(){
-  	},
-  	componentWillUnmount: function(){
-  	},
-  	componentDidMount: function(){
-  	},
-  	getInitialState: function(){
-    	return {
-	      trueSwitchIsOn: true,
-	      falseSwitchIsOn: false,
-	    };
-  	},
-	render: function() {
-	    return (
-	      <View>
-		    	<Text>
-		    		Bogus
-		     	</Text>
-		    	<SwitchIOS
-		        	onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
-		        	style={{marginBottom: 10}}
-		        	value={this.state.falseSwitchIsOn} />
-		    	<SwitchIOS
-		        	onValueChange={(value) => this.setState({trueSwitchIsOn: value})}
-		        	value={this.state.trueSwitchIsOn} />
-	      </View>
-	      <View>
-	        <SwitchIOS
+  componentWillMount: function(){
+  },
+  componentWillUnmount: function(){
+  },
+  componentDidMount: function(){
+  },
+  getInitialState: function(){
+    return {
+      trueSwitchIsOn: true,
+      falseSwitchIsOn: false,
+    };
+  },
+  render: function() {
+    return (
+      <View>
+        <View>
+          <Text>
+            Bogus
+          </Text>
+          <SwitchIOS
+            onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
+            style={{marginBottom: 10}}
+            value={this.state.falseSwitchIsOn} />
+          <SwitchIOS
+            onValueChange={(value) => this.setState({trueSwitchIsOn: value})}
+            value={this.state.trueSwitchIsOn} />
+        </View>
+        <View>
+          <SwitchIOS
 	          onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
-	          style={{marginBottom: 10}}
-	          value={this.state.falseSwitchIsOn} />
-	        <SwitchIOS
-	          onValueChange={(value) => this.setState({trueSwitchIsOn: value})}
-	          value={this.state.trueSwitchIsOn} />
-	      </View>
-	    );
-  	}
+            style={{marginBottom: 10}}
+            value={this.state.falseSwitchIsOn} />
+          <SwitchIOS
+            onValueChange={(value) => this.setState({trueSwitchIsOn: value})}
+            value={this.state.trueSwitchIsOn} />
+        </View>
+      </View>
+    );
+  }
 });
 
 module.exports = Settings;
