b71159b00cab37697d935d85ac23655cd80fac78
Eugene Lee <colemakdvorak@gmail.com>
2015-10-19 15:59:25 -0700

    (UI) Made home swipe global

--- viapp/jsx/components/Home/Home.js
+++ viapp/jsx/components/Home/Home.js
@@ -1,6 +1,9 @@
 const React = require('react-native');
-const Palette = require('../styles/Palette');
-const ReboundBall = require('./ReboundBall.js')
+const Palette = require('../styles/Palette'); 
+const Dimensions = require('Dimensions');
+const windowSize = Dimensions.get('window');
+
+const CIRCLE_SIZE = windowSize.width*0.85;
 
 const {
   StyleSheet,
@@ -8,17 +11,81 @@ const {
   View,
   TouchableHighlight,
   Image,
+  Animated,
+  PanResponder,
 } = React;
 
 class Home extends React.Component {
   constructor(props) {
     super();
+    this.state = {
+        listening: false,
+        pan: new Animated.ValueXY()
+    };
+  }
+
+  componentWillMount() {
+    this._animatedValueX = 0;
+    this._animatedValueY = 0; 
+    this.state.pan.x.addListener((value) => this._animatedValueX = value.value);
+    this.state.pan.y.addListener((value) => this._animatedValueY = value.value);
+    
+        this._panResponder = PanResponder.create({
+          onMoveShouldSetResponderCapture: () => true,
+          onMoveShouldSetPanResponderCapture: () => true,
+          onPanResponderGrant: (e, gestureState) => {
+            console.log('hit');
+            this.state.pan.setOffset({x: this._animatedValueX, y: this._animatedValueY});
+            this.state.pan.setValue({x: 0, y: 0});
+          },
+          onPanResponderMove: Animated.event([
+                null, {dx: this.state.pan.x, dy: this.state.pan.y},
+          ]),
+          onPanResponderRelease: () => {
+            this.state.listening = !this.state.listening;
+            console.log(this.state.listening);
+            Animated.spring(this.state.pan, {
+              toValue: 0
+            }).start();
+
+            if(this.state.listening){
+              
+            }
+          }
+        });
+  }
+
+  componentWillUnmount() {
+    this.state.pan.x.removeAllListeners();  
+    this.state.pan.y.removeAllListeners();
+  }
+
+  getStyle() {
+    return [
+              styles.circle, 
+              {
+                transform: [
+                  {
+                    translateX: this.state.pan.x
+                  },
+                  {
+                    translateY: this.state.pan.y
+                  },
+                ]
+              },
+              {
+                opacity: this.state.pan.y.interpolate({inputRange: [-300, 0, 300], outputRange: [0.5, 1, 0.5]})
+              }
+            ];
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <ReboundBall />
+      <View style={styles.container}
+       {...this._panResponder.panHandlers}>
+        <Animated.View 
+          style={this.getStyle()}
+        />
       </View>
     );
   }
@@ -27,38 +94,20 @@ class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //backgroundColor: '#000000',
-    flexDirection: 'column',
-    //justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: 'flex-end',
     paddingLeft: 0,
-    marginLeft: 0
+    marginLeft: 0,
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  backdropView: {
-    height: 250,
-    width: 250,
-    marginLeft: 65,
-    borderColor: 'rgba(255,255,255,0.6)',
-    borderWidth: 2,
-    borderRadius: 250,
-    backgroundColor: 'rgba(0,0,0,0)',
-  },
-  inner: {
-    marginTop: 50,
-  },
-  title: {
-    fontSize: 50,
-    textAlign: 'center',
-    backgroundColor: 'rgba(0,0,0,0)',
-    color: 'white',
-  },
-  spoken: {
-    marginTop: 10,
-    fontSize: 18,
-    textAlign: 'center',
-    backgroundColor: 'rgba(0,0,0,0)',
-    color: 'white',
-  },
+  circle: {
+    backgroundColor: 'black',
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE/2,
+    backgroundColor: 'blue',
+    marginBottom: -CIRCLE_SIZE/2,
+  } 
 });
 
 module.exports = Home;
